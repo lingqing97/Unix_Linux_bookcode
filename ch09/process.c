09e12ed0ac2bfd7877e1ad9076deb465b1c5bd35
wushukun <1766552041@qq.com>
2021-03-01 10:59:40 +0800

    :memo: 编写chapter9部分代码

--- ch09/process.c
+++ ch09/process.c
@@ -4,12 +4,17 @@
 int is_control_command(char*);
 int do_control_comand(char**);
 int ok_to_execute();
+int builtin_commend(char** args,int* resultp);
 
 int process(char** args)
 {
     int rv=0;
     if(args[0]==NULL) rv=0;
     else if(is_control_command(args[0])) rv=do_control_comand(args);
-    else if(ok_to_execute()) rv=execute(args);
+    else if(ok_to_execute()) {
+        if(!builtin_commend(args,&rv)){
+            rv=execute(args);
+        }
+    }
     return rv;
 }
